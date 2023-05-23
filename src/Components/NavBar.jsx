f9f21810f1df7cfe4cac1274727c0f1ae5c400c6
Na-Ku <naku3571@gmail.com>
2023-05-23 08:44:00 +0700

    langkah pertama & membuat navigation bar yang belum jadi

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Pages/NaviBar/Navigation';
+
+const NavBar = () => {
+    return (
+        <div>
+            <Navigation/>
+        </div>
+    );
+}
+
+export default NavBar;
