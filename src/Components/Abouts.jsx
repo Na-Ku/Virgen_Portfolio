db538cb07606df3d2a0d992904f0e04c1b7161a9
Na-Ku <naku3571@gmail.com>
2023-06-15 05:06:58 +0700

    kerangka footer sudah jadi

--- src/Components/Abouts.jsx
+++ src/Components/Abouts.jsx
@@ -3,39 +3,47 @@ import React from "react";
 const Abouts = () => {
   return (
     <div className="mt-5">
-      <ul>
-        <h3>Warung Online</h3>
-        <li>
-          <a href="#f">Tentang Warung Online</a>
-        </li>
-        <li>
-          <a href="#g">Blog</a>
-        </li>
-        <li>
-          <a href="#g">Mitra</a>
-        </li>
-      </ul>
-      <ul>
-        <h3>Belanja</h3>
-        <li>
-          <a href="#f">Tagihan & Top-up</a>
-        </li>
-        <li>
-          <a href="#g">Promo</a>
-        </li>
-        <li>
-          <a href="#g">All Product</a>
-        </li>
-      </ul>
-      <ul>
-        <h3>Follow Us</h3>
-        <li>
-          <a href="#f">IG</a>
-        </li>
-        <li>
-          <a href="#g">Twiter</a>
-        </li>
-      </ul>
+      <div className="row align-items-center">
+        <div className="col">
+          <ul>
+            <h4>Warung Online</h4>
+            <li>
+              <a href="#f">Tentang Warung Online</a>
+            </li>
+            <li>
+              <a href="#g">Blog</a>
+            </li>
+            <li>
+              <a href="#g">Mitra</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <ul>
+            <h4>Belanja</h4>
+            <li>
+              <a href="#f">Tagihan & Top-up</a>
+            </li>
+            <li>
+              <a href="#g">Promo</a>
+            </li>
+            <li>
+              <a href="#g">All Product</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <ul>
+            <h4>Follow Us</h4>
+            <li>
+              <a href="#f">IG</a>
+            </li>
+            <li>
+              <a href="#g">Twiter</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
