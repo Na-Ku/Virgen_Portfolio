66d154fc453cd30604264a24b1bc3c1d31c99f55
Na-Ku <naku3571@gmail.com>
2023-06-19 09:45:12 +0700

    memulai untuk membuat porto

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "./Components/NavBar";
-import CarouselComponent from "./Pages/CarouselBanner/CarouselComponent";
-import AllCategories from "./Pages/Category/AllCategories";
-import Promo from "./Pages/Diskon/Promo";
-import FrontProduct from "./Pages/Products/FrontProduct";
-import About from "./Pages/Footer/About";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
-      <main style={{ height: "1000px" }}>
-        <div className="bg-warning bg-opacity-50">
-          <CarouselComponent />
-        </div>
-        <span>
-          <AllCategories />
-        </span>
-        <span>
-          <Promo />
-        </span>
-        <span>
-          <FrontProduct />
-        </span>
-        <footer>
-          <About />
-        </footer>
-      </main>
+      <h1>Mulai</h1>
     </div>
   );
 }
