8dc20759b9e4dc2a78a480e542d22d44ba4a4667
Na-Ku <naku3571@gmail.com>
2023-06-23 15:57:15 +0700

    progres pengerjaan 'home' 25%, selanjutnya masuk ke bagian css-nya

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Social from "./Social";
+import Data from "./Data";
 
 const Home = () => {
   return (
-    <div>
-      <p>Mari Mulai</p>
-    </div>
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+          <div className="home__img"></div>
+          <Data />
+        </div>
+      </div>
+    </section>
   );
 };
 
