c01df759079683be65786c9b347a91c8474c41bc
Na-Ku <naku3571@gmail.com>
2023-06-26 16:51:38 +0700

    bagian About selesai

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,7 +3,23 @@ import React from "react";
 const Info = () => {
   return (
     <div>
-      <p>oke</p>
+      <div className="about__info grid">
+        <div className="about__box">
+          <i class="bx bx-award"></i>
+          <h3 className="about__title">Experience</h3>
+          <span className="about__subtitle">Junior Developer</span>
+        </div>
+        <div className="about__box">
+          <i class="bx bx-briefcase-alt"></i>
+          <h3 className="about__title">Experience</h3>
+          <span className="about__subtitle">Junior Developer</span>
+        </div>
+        <div className="about__box">
+          <i class="bx bx-support"></i>
+          <h3 className="about__title">Experience</h3>
+          <span className="about__subtitle">Junior Developer</span>
+        </div>
+      </div>
     </div>
   );
 };
